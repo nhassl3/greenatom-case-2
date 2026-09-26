d51799451f7f233c92a85b89e049cf01f8f890b8
nhassl3 <matvey.kvasov.05@mail.ru>
2026-09-26 15:04:37 +0300

    feat: added config for CORD policy

---
+++ src/config/cors.ts
@@ -0,0 +1,14 @@
+import { CorsOrigins } from '@src/common/constants/env'
+import { CrossEnvOptions } from 'cross-env'
+import { CorsOptions } from 'cors';
+
+export const corsOptions: CorsOptions = {
+	origin(origin, cb) {
+		if (!origin || CorsOrigins.includes(origin)) return cb(null, true);
+		cb(null, false);
+	},
+	methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+	allowedHeaders: ['Content-Type', 'X-Request-Id', 'X-API-Key'],
+	exposedHeaders: ['X-Request-Id', 'Location', 'RateLimit', 'RateLimit-Policy', 'Retry-After'],
+	maxAge: 600,
+};
